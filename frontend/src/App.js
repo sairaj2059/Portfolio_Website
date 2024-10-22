133c9d8bca82b51aabb275380c0d4c415852a7f9
sairaj2059 <rajguragai124@gmail.com>
2024-10-22 15:38:22 +0530

    Updated Version

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Dashboard />
+      </>
+    </Router>
+  );
+}
+
+export default App;
